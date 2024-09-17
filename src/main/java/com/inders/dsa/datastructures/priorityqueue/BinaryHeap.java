7640573d7bebbd7c1a5c7eb3df0b3bfcfb21f892
Inderjeet Singh <inderjeetsingh9646@gmail.com>
2024-09-17 11:53:50 +0530

    add min binary heap

---
+++ src/main/java/com/inders/dsa/datastructures/priorityqueue/BinaryHeap.java
@@ -0,0 +1,206 @@
+package com.inders.dsa.datastructures.priorityqueue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public final class BinaryHeap<T extends Comparable<T>> {
+
+    // list to store elements in the heap
+    private List<T> heap;
+
+    public BinaryHeap() {
+        this(1);
+    }
+
+    // initialize heap with a certain capacity
+    public BinaryHeap(int capacity) {
+        heap = new ArrayList<>(capacity);
+    }
+
+    // construct a heap using heapify in O(n) linear time
+    public BinaryHeap(T[] elements) {
+        int capacity = elements.length;
+        heap = new ArrayList<>(capacity);
+
+        // add all elements to the heap
+        heap.addAll(Arrays.asList(elements).subList(0, capacity));
+
+        // heapify process : http://www.cs.umd.edu/~meesh/351/mount/lectures/lect14-heapsort-analysis-part.pdf
+        for (int i = Math.max(0, (capacity / 2) - 1); i >= 0; i--) {
+            sink(i);
+        }
+    }
+
+    public BinaryHeap(Collection<T> elements) {
+        int capacity = elements.size();
+        heap = new ArrayList<>(capacity);
+
+        // add all elements to the heap
+        heap.addAll(elements);
+
+        // heapify process : http://www.cs.umd.edu/~meesh/351/mount/lectures/lect14-heapsort-analysis-part.pdf
+        for (int i = Math.max(0, (capacity / 2) - 1); i >= 0; i--) {
+            sink(i);
+        }
+    }
+
+    public int size() {
+        return heap.size();
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public void clear() {
+        heap.clear();
+    }
+
+    // returns root element of the heap or
+    // with lowest priority in this priority queue
+    // null is returned if heap is empty
+    public T peek() {
+        if (isEmpty()) {
+            return null;
+        }
+        return heap.getFirst();
+    }
+
+    // removes and returns root element of the heap
+    public T poll() {
+        if (isEmpty()) {
+            return null;
+        }
+        return removeAt(0);
+    }
+
+    public boolean contains(T elem) {
+        return heap.contains(elem);
+    }
+
+    // add an element to the heap
+    public void add(T elem) {
+        if (elem == null) {
+            throw new IllegalArgumentException();
+        }
+        heap.add(elem);
+        swim(size() - 1);
+    }
+
+    // remove first occurrence of an element from the heap
+    public boolean remove(T elem) {
+        if (elem == null) {
+            return false;
+        }
+
+        for (int i = 0; i < size(); i++) {
+            if (elem.equals(heap.get(i))) {
+                removeAt(i);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private T removeAt(int i) {
+        if (isEmpty()) {
+            return null;
+        }
+
+        int lastIndex = size() - 1;
+        T removedElem = heap.get(i);
+
+        swap(i, lastIndex); // swap elem at i with last elem
+        heap.remove(lastIndex); // clear the last index
+
+        if (i == lastIndex) {
+            return removedElem;
+        }
+
+        T elem = heap.get(i);
+        sink(i); // try sinking
+        if (heap.get(i).equals(elem)) {
+            swim(i); // swim if sinking did not work
+        }
+
+        return removedElem;
+    }
+
+    private void swim(int k) {
+        int parent = (k - 1) / 2;
+
+        while (k > 0 && less(k, parent)) {
+            swap(k, parent);
+            k = parent;
+            parent = (k - 1) / 2;
+        }
+    }
+
+    private void sink(int k) {
+        int size = size();
+        while (true) {
+            int left = 2 * k + 1;
+            int right = 2 * k + 2;
+            int smallest = left; // assume left node is the smallest
+
+            // check is right is smaller
+            if (right < size && less(right, smallest)) {
+                smallest = right;
+            }
+
+            // break out of loop if out of bound of heap or k is smallest
+            if (left >= size || less(k, smallest)) {
+                break;
+            }
+
+            // Move down the tree with smallest node
+            swap(k, smallest);
+            k = smallest;
+        }
+    }
+
+    // Tests if the value of node i <= node j
+    // This method assumes i & j are valid indices, O(1)
+    private boolean less(int i, int j) {
+        T node1 = heap.get(i);
+        T node2 = heap.get(j);
+        return node1.compareTo(node2) <= 0;
+    }
+
+    // Swap two nodes. Assumes i & j are valid, O(1)
+    private void swap(int i, int j) {
+        T elem_i = heap.get(i);
+        T elem_j = heap.get(j);
+
+        heap.set(i, elem_j);
+        heap.set(j, elem_i);
+    }
+
+    // recursively check if the min heap invariant
+    // is valid for this heap
+    public boolean isMinHeap(int k) {
+        // If we are outside the bounds of the heap return true
+        int heapSize = size();
+        if (k >= heapSize) return true;
+
+        int left = 2 * k + 1;
+        int right = 2 * k + 2;
+
+        // Make sure that the current node k is less than
+        // both of its children left, and right if they exist
+        // return false otherwise to indicate an invalid heap
+        if (left < heapSize && !less(k, left)) return false;
+        if (right < heapSize && !less(k, right)) return false;
+
+        // Recurse on both children to make sure they're also valid heaps
+        return isMinHeap(left) && isMinHeap(right);
+    }
+
+    @Override
+    public String toString() {
+        return heap.toString();
+    }
+}
---
+++ src/test/java/com/inders/dsa/datastructures/priorityqueue/BinaryHeapTest.java
@@ -0,0 +1,330 @@
+package com.inders.dsa.datastructures.priorityqueue;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BinaryHeapTest {
+
+    static final int LOOPS = 100;
+    static final int MAX_SZ = 100;
+
+    @Test
+    public void testEmpty() {
+        BinaryHeap<Integer> q = new BinaryHeap<>();
+        assertEquals(0, q.size());
+        assertTrue(q.isEmpty());
+        assertNull(q.poll());
+        assertNull(q.peek());
+    }
+
+    @Test
+    public void testHeapProperty() {
+
+        BinaryHeap<Integer> q = new BinaryHeap<>();
+        Integer[] nums = {3, 2, 5, 6, 7, 9, 4, 8, 1};
+
+        // Try manually creating heap
+        for (int n : nums) {
+            q.add(n);
+        }
+        for (int i = 1; i <= 9; i++) {
+            assertEquals(i, q.poll());
+        }
+
+        q.clear();
+
+        // Try heapify constructor
+        q = new BinaryHeap<>(nums);
+        for (int i = 1; i <= 9; i++) {
+            assertEquals(i, q.poll());
+        }
+    }
+
+    @Test
+    public void testHeapify() {
+
+        for (int i = 1; i < LOOPS; i++) {
+
+            Integer[] lst = genRandArray(i);
+            BinaryHeap<Integer> pq = new BinaryHeap<>(lst);
+
+            PriorityQueue<Integer> pq2 = new PriorityQueue<>(i);
+            pq2.addAll(Arrays.asList(lst));
+
+            assertTrue(pq.isMinHeap(0));
+            while (!pq2.isEmpty()) {
+                assertEquals(pq2.poll(), pq.poll());
+            }
+        }
+    }
+
+    @Test
+    public void testClear() {
+
+        BinaryHeap<String> q;
+        String[] strs = {"aa", "bb", "cc", "dd", "ee"};
+        q = new BinaryHeap<>(strs);
+        q.clear();
+        assertEquals(0, q.size());
+        assertTrue(q.isEmpty());
+    }
+
+    @Test
+    public void testContainment() {
+
+        String[] strs = {"aa", "bb", "cc", "dd", "ee"};
+        BinaryHeap<String> q = new BinaryHeap<>(strs);
+        q.remove("aa");
+        assertFalse(q.contains("aa"));
+        q.remove("bb");
+        assertFalse(q.contains("bb"));
+        q.remove("cc");
+        assertFalse(q.contains("cc"));
+        q.remove("dd");
+        assertFalse(q.contains("dd"));
+        q.clear();
+        assertFalse(q.contains("ee"));
+    }
+
+    @Test
+    public void testContainmentRandomized() {
+
+        for (int i = 0; i < LOOPS; i++) {
+
+            List<Integer> randNums = genRandList(100);
+            PriorityQueue<Integer> PQ = new PriorityQueue<>();
+            BinaryHeap<Integer> pq = new BinaryHeap<>();
+            for (int j = 0; j < randNums.size(); j++) {
+                pq.add(randNums.get(j));
+                PQ.add(randNums.get(j));
+            }
+
+            for (int j = 0; j < randNums.size(); j++) {
+
+                int randVal = randNums.get(j);
+                assertEquals(PQ.contains(randVal), pq.contains(randVal));
+                pq.remove(randVal);
+                PQ.remove(randVal);
+                assertEquals(PQ.contains(randVal), pq.contains(randVal));
+            }
+        }
+    }
+
+    public void sequentialRemoving(Integer[] in, Integer[] removeOrder) {
+
+        assertEquals(removeOrder.length, in.length);
+
+        BinaryHeap<Integer> pq = new BinaryHeap<>(in);
+        PriorityQueue<Integer> PQ = new PriorityQueue<>();
+        for (int value : in) PQ.offer(value);
+
+        assertTrue(pq.isMinHeap(0));
+
+        for (int i = 0; i < removeOrder.length; i++) {
+
+            int elem = removeOrder[i];
+
+            assertEquals(PQ.peek(), pq.peek());
+            assertEquals(PQ.remove(elem), pq.remove(elem));
+            assertEquals(PQ.size(), pq.size());
+            assertTrue(pq.isMinHeap(0));
+        }
+
+        assertTrue(pq.isEmpty());
+    }
+
+    @Test
+    public void testRemoving() {
+
+        Integer[] in = {1, 2, 3, 4, 5, 6, 7};
+        Integer[] removeOrder = {1, 3, 6, 4, 5, 7, 2};
+        sequentialRemoving(in, removeOrder);
+
+        in = new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        removeOrder = new Integer[] {7, 4, 6, 10, 2, 5, 11, 3, 1, 8, 9};
+        sequentialRemoving(in, removeOrder);
+
+        in = new Integer[] {8, 1, 3, 3, 5, 3};
+        removeOrder = new Integer[] {3, 3, 5, 8, 1, 3};
+        sequentialRemoving(in, removeOrder);
+
+        in = new Integer[] {7, 7, 3, 1, 1, 2};
+        removeOrder = new Integer[] {2, 7, 1, 3, 7, 1};
+        sequentialRemoving(in, removeOrder);
+
+        in = new Integer[] {32, 66, 93, 42, 41, 91, 54, 64, 9, 35};
+        removeOrder = new Integer[] {64, 93, 54, 41, 35, 9, 66, 42, 32, 91};
+        sequentialRemoving(in, removeOrder);
+    }
+
+    @Test
+    public void testRemovingDuplicates() {
+
+        Integer[] in = new Integer[] {2, 7, 2, 11, 7, 13, 2};
+        BinaryHeap<Integer> pq = new BinaryHeap<>(in);
+
+        assertEquals(2, pq.peek());
+        pq.add(3);
+
+        assertEquals(2, pq.poll());
+        assertEquals(2, pq.poll());
+        assertEquals(2, pq.poll());
+        assertEquals(3, pq.poll());
+        assertEquals(7, pq.poll());
+        assertEquals(7, pq.poll());
+        assertEquals(11, pq.poll());
+        assertEquals(13, pq.poll());
+    }
+
+    @Test
+    public void testRandomizedPolling() {
+
+        for (int i = 0; i < LOOPS; i++) {
+
+            int sz = i;
+            List<Integer> randNums = genRandList(sz);
+            PriorityQueue<Integer> pq1 = new PriorityQueue<>();
+            BinaryHeap<Integer> pq2 = new BinaryHeap<>();
+
+            // Add all the elements to both priority queues
+            for (Integer value : randNums) {
+                pq1.offer(value);
+                pq2.add(value);
+            }
+
+            while (!pq1.isEmpty()) {
+
+                assertTrue(pq2.isMinHeap(0));
+                assertEquals(pq2.size(), pq1.size());
+                assertEquals(pq2.peek(), pq1.peek());
+                assertEquals(pq2.contains(pq2.peek()), pq1.contains(pq1.peek()));
+
+                Integer v1 = pq1.poll();
+                Integer v2 = pq2.poll();
+
+                assertEquals(v2, v1);
+                assertEquals(pq2.peek(), pq1.peek());
+                assertEquals(pq2.size(), pq1.size());
+                assertTrue(pq2.isMinHeap(0));
+            }
+        }
+    }
+
+    @Test
+    public void testRandomizedRemoving() {
+
+        for (int i = 0; i < LOOPS; i++) {
+
+            int sz = i;
+            List<Integer> randNums = genRandList(sz);
+            PriorityQueue<Integer> pq1 = new PriorityQueue<>();
+            BinaryHeap<Integer> pq2 = new BinaryHeap<>();
+
+            // Add all the elements to both priority queues
+            for (Integer value : randNums) {
+                pq1.offer(value);
+                pq2.add(value);
+            }
+
+            Collections.shuffle(randNums);
+            int index = 0;
+
+            while (!pq1.isEmpty()) {
+
+                int removeNum = randNums.get(index++);
+
+                assertTrue(pq2.isMinHeap(0));
+                assertEquals(pq2.size(), pq1.size());
+                assertEquals(pq2.peek(), pq1.peek());
+                pq1.remove(removeNum);
+                pq2.remove(removeNum);
+                assertEquals(pq2.peek(), pq1.peek());
+                assertEquals(pq2.size(), pq1.size());
+                assertTrue(pq2.isMinHeap(0));
+            }
+        }
+    }
+
+    @Test
+    public void testPQReusability() {
+
+        List<Integer> SZs = genUniqueRandList(LOOPS);
+
+        PriorityQueue<Integer> PQ = new PriorityQueue<>();
+        BinaryHeap<Integer> pq = new BinaryHeap<>();
+
+        for (int sz : SZs) {
+
+            pq.clear();
+            PQ.clear();
+
+            List<Integer> nums = genRandList(sz);
+            for (int n : nums) {
+                pq.add(n);
+                PQ.add(n);
+            }
+
+            Collections.shuffle(nums);
+
+            for (int i = 0; i < sz / 2; i++) {
+
+                // Sometimes add a new number into the BinaryHeap
+                if (0.25 < Math.random()) {
+                    int randNum = (int) (Math.random() * 10000);
+                    PQ.add(randNum);
+                    pq.add(randNum);
+                }
+
+                int removeNum = nums.get(i);
+
+                assertTrue(pq.isMinHeap(0));
+                assertEquals(pq.size(), PQ.size());
+                assertEquals(pq.peek(), PQ.peek());
+
+                PQ.remove(removeNum);
+                pq.remove(removeNum);
+
+                assertEquals(pq.peek(), PQ.peek());
+                assertEquals(pq.size(), PQ.size());
+                assertTrue(pq.isMinHeap(0));
+            }
+        }
+    }
+
+    static Integer[] genRandArray(int sz) {
+        Integer[] lst = new Integer[sz];
+        for (int i = 0; i < sz; i++) {
+            lst[i] = (int) (Math.random() * MAX_SZ);
+        }
+        return lst;
+    }
+
+    // Generate a list of random numbers
+    static List<Integer> genRandList(int sz) {
+        List<Integer> lst = new ArrayList<>(sz);
+        for (int i = 0; i < sz; i++) {
+            lst.add((int) (Math.random() * MAX_SZ));
+        }
+        return lst;
+    }
+
+    // Generate a list of unique random numbers
+    static List<Integer> genUniqueRandList(int sz) {
+        List<Integer> lst = new ArrayList<>(sz);
+        for (int i = 0; i < sz; i++) {
+            lst.add(i);
+        }
+        Collections.shuffle(lst);
+        return lst;
+    }
+}
